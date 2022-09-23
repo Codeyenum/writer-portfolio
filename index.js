1d9fb99d88969bd6e71be7fa51b67810c8242a78
Abanum Olise <abanum00@gmail.com>
2022-09-23 22:12:55 +0100

    theme toggle functionality cleanup

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let menuButton = document.querySelector("#menu-button");
 let closeButton = document.querySelector("#close-button");
+
 let lightBox = document.querySelector(".mobile-nav-lightbox");
 let mobileNav = document.querySelector(".mobile-nav");
-let toggleSwitch = document.querySelector(".toggle-button");
+
+let toggleButton = document.querySelector(".toggle-button");
 
 menuButton.addEventListener("click", () => {
     lightBox.classList.toggle("hide");
@@ -14,63 +16,71 @@ closeButton.addEventListener("click", () => {
     mobileNav.classList.toggle("hide");
 })
 
-toggleSwitch.addEventListener("click", () => {
-    if (toggleSwitch.checked) {
-        document.body.classList.add("dark");
-    } else {
-        document.body.classList.remove("dark");
-    }
-})
-
 function detectColorScheme() {
-    var theme = "light";    //default to light
-
+    let theme = "light";    //default to light
     let getTheme = localStorage.getItem("theme");
+
     //local storage is used to override OS theme settings
-
-        if (getTheme == "dark") {
-            var theme = "dark";
-
-    } else if (!window.matchMedia) {
-        //matchMedia method not supported
-        return false;
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        //OS theme setting detected as dark
-        var theme = "dark";
+    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches) {
+        theme = "light";
+    } else if (getTheme == "dark") {
+        theme = "dark";
+    } else if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        theme = "dark";
+    } else {
+        //OS theme setting detected as light
+        theme = "light";
     }
 
-    //dark theme preferred, set document with a `data-theme` attribute
+    //dark theme preferred, set document with a `data-theme` attribute    
     if (theme == "dark") {
         document.documentElement.setAttribute("data-theme", "dark");
     }
 }
 detectColorScheme();
 
-//identify the toggle switch HTML element
+//pre-check the dark-theme checkbox if dark-theme is set
+if (document.documentElement.getAttribute("data-theme") == "dark") {
+    setToDarkMode()
+} else {
+    setToLightMode();
+}
 
-//function that changes the theme, and sets a localStorage variable to track the theme between page loads
+//function that tracks the dark theme between page loads with a localStorage variable 
+//sets the toggle button
+//and sets the dark theme styling
+function setToDarkMode() {
+    localStorage.setItem('theme', 'dark');
+    document.documentElement.setAttribute('data-theme', 'dark');
+    toggleButton.checked = true;
+    document.body.classList.add("dark");
+}
+
+//function that tracks the light theme between page loads with a localStorage variable 
+//sets the toggle button
+//and sets the light theme styling
+function setToLightMode() {
+    localStorage.setItem('theme', 'light');
+    document.documentElement.setAttribute('data-theme', 'light');
+    toggleButton.checked = false;
+    document.body.classList.remove("dark");
+}
+
+//function that evaluates theme switching
 function switchTheme(e) {
     if (e.target.checked) {
-        localStorage.setItem('theme', 'dark');
-        document.documentElement.setAttribute('data-theme', 'dark');
-        toggleSwitch.checked = true;
+        setToDarkMode();
     } else {
-        localStorage.setItem('theme', 'light');
-        document.documentElement.setAttribute('data-theme', 'light');
-        toggleSwitch.checked = false;
+        setToLightMode();
     }
 }
 
 //listener for changing themes
-toggleSwitch.addEventListener('change', switchTheme, false);
+toggleButton.addEventListener('change', switchTheme, false);
 
-//pre-check the dark-theme checkbox if dark-theme is set
-if (document.documentElement.getAttribute("data-theme") == "dark") {
-    toggleSwitch.checked = true;
-}
-
-if (toggleSwitch.checked) {
-    document.body.classList.add("dark");
-} else {
-    document.body.classList.remove("dark");
-}
+// detectColorScheme checks if preferred theme is dark
+// if it is, it sets the data-theme variable to dark 
+// which then causes the site theme to be set to dark theme
+// if it isn't, it sets the site theme to light theme
+// setToLightMode sets the data-theme variable to light
+// subsequent checks then work with either dark or light data-themes
